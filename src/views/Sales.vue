11a3e3318b271d8d7dc1e330526a2637834a7403
Stephen Onnen <stephen.onnen@gmail.com>
2021-05-31 03:50:04 -0500

    updated sales fonts

--- src/views/Sales.vue
+++ src/views/Sales.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex justify-center">
     <div class="flex flex-auto flex-col max-w-screen-xl text-left p-6">
-      <h1 class="font-bold text-6xl pb-6 font-roboto">Sales</h1>
+      <h1 class="font-black text-6xl pb-6 font-roboto">Sales</h1>
       <div class="text-lg pb-2">Sales are now open!</div>
-      <h2 class="font-bold text-2xl py-2 font-roboto">Sales Contact:</h2>
+      <h2 class="font-black text-2xl py-2 font-roboto">Sales Contact:</h2>
       <div class="flex flex-col justify-items-start">
         <div class="flex flex-col items-start">
           <div class="font-bold pr-2">Discord:</div>
@@ -17,7 +17,7 @@
       <div class="py-4 text-lg">
         We only accept gold on the server Moon Guard!
       </div>
-      <div class="text-4xl font-bold py-4 font-roboto">Current Offerings</div>
+      <div class="text-4xl font-black py-4 font-roboto">Current Offerings</div>
       <CastleNathria />
     </div>
   </div>
