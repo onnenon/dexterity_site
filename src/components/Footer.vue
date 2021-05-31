77f1974d0cd5f169839eb59323d59225e7f3c1ab
Stephen Onnen <stephen.onnen@gmail.com>
2021-05-31 03:42:23 -0500

    Added footer links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="flex justify-center bg-gray-800">
-    <div class="flex flex-auto flex-row max-w-screen-xl">
-      <div class="w-1/3"></div>
-      <div class="w-1/3"></div>
-      <div class="w-1/3">
-        <div>Contact Us:</div>
-        <div>Email:</div>
+  <div
+    class="
+      flex flex-auto
+      items-center
+      justify-center
+      p-16
+      mt-20
+      bg-gray-100
+      text-gray-400
+    "
+  >
+    <div class="flex flex-wrap flex-auto flex-row max-w-screen-xl">
+      <div class="foot-third"></div>
+      <div class="foot-third"></div>
+      <div class="foot-third">
+        <div class="text-gray-900 font-semibold pb-4 text-xs text-roboto">
+          LINKS
+        </div>
+        <a
+          class="foot-text foot-link"
+          href="https://worldofwarcraft.com/en-us/guild/us/moon-guard/dexterity"
+          alt="Armory Link"
+          target="_blank"
+          >Armory</a
+        >
+        <a
+          class="foot-text foot-link"
+          href="https://www.wowprogress.com/guild/us/moon-guard/Dexterity"
+          alt="WoWProgress Link"
+          target="_blank"
+          >WoWProgress</a
+        >
       </div>
     </div>
   </div>
@@ -15,8 +40,18 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'footer',
+  name: 'foot',
 })
 </script>
 
-<style></style>
+<style>
+.foot-text {
+  @apply flex flex-auto font-medium text-sm pb-4 font-roboto;
+}
+.foot-link {
+  @apply hover:text-gray-900;
+}
+.foot-third {
+  @apply flex flex-auto flex-col md:w-1/3 justify-center px-10;
+}
+</style>
